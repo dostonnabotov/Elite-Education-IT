7a92a05e694a94655701183ba3b8f8c3a051c9d8
Doston Nabotov <dostonnabotov@gmail.com>
2024-06-05 19:55:52 +0500

    Add "Lesson 66"

---
+++ Lesson 66/script.js
@@ -0,0 +1,30 @@
+// variable
+const id = "AA12345678";
+let student = "Amirbek";
+let yosh = 15;
+let bitiruvchi = false;
+
+console.log(yosh);
+console.log(student);
+console.log(id);
+
+yosh = 18;
+student = "Sardor";
+// id = "CC0000000"; // Xato kod!
+
+console.log(yosh);
+console.log(student + yosh);
+console.log(id);
+
+// console.log(id); // AA12345678
+// console.log(student); // Amirbek
+// console.log(yosh + 5); // 20
+// console.log(student + 10); // Amirbek10
+// console.log(id - 10); // NaN
+// console.log(student + id); // AmirbekAA12345678
+// console.log(bitiruvchi); // false
+// console.log(student + bitiruvchi); // Amirbekfalse
+// console.log(id - bitiruvchi); // NaN
+
+// console.log(35 - 17);
+// console.log(5 * 12);
