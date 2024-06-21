06e5e0ed87b49ede22825b900a9d402f7c8f51dd
Doston Nabotov <dostonnabotov@gmail.com>
2024-06-21 20:07:35 +0500

    Add "Lesson 70"

---
+++ Lesson 70/script.js
@@ -0,0 +1,47 @@
+// Yomon usuli
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// Yaxshi usuli
+// for (let raqam = 1; raqam <= 15; raqam += 5) {
+//   console.log(raqam); // 1, 6, 11
+// }
+
+let studentlar = ["Amir", "Abdumalik", "Sardor", "Temur"];
+
+// Yomon usuli
+// console.log("Salom " + studentlar[0]); // Amir
+// console.log("Salom " + studentlar[1]); // Abdumalik
+// console.log("Salom " + studentlar[2]); // Sardor
+
+// Yaxshi usuli
+// for (let studentJoyi = 0; studentJoyi <= 3; studentJoyi++) {
+//   console.log(studentlar[studentJoyi]);
+// }
+
+// Ko'paytirish jadvali - 9
+// Yomon usuli
+// console.log(9 * 1);
+// console.log(9 * 2);
+// console.log(9 * 3);
+// console.log(9 * 4);
+// console.log(9 * 5);
+// console.log(9 * 6);
+// console.log(9 * 7);
+// console.log(9 * 8);
+// console.log(9 * 9);
+// console.log(9 * 10);
+
+// Yaxshi usuli
+for (let raqam = 1; raqam <= 10; raqam += 1) {
+  // console.log(9 * raqam);
+  console.log(`9 * ${raqam} = ${9 * raqam}`);
+}
